d99aab74a0cc39108d96613e2fbc113f2978145e
Bishal299 <bishalkhuntia23@gmail.com>
2026-06-25 20:12:48 +0530

    Time: 0 ms (100%), Space: 8.7 MB (22.81%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,9 +3,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string a=to_string(x);
-        string b=a;
-        reverse(b.begin(),b.end());
-        return a==b; 
+        if(x<0) return false;
+          int org=x;
+          long long rev=0;
+          while(x>0){
+            rev=(rev*10)+(x%10);
+            x=x/10;
+          }
+          return rev==org;
+
     }
 };
