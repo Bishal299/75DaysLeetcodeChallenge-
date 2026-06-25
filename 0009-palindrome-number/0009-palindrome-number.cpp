516964c09b006b3c19416abfcc02e212ecaf8288
Bishal299 <bishalkhuntia23@gmail.com>
2026-06-25 20:08:21 +0530

    Time: 7 ms (14.73%), Space: 10.9 MB (13.71%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include<algorithm>
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string a=to_string(x);
+        string b=a;
+        reverse(b.begin(),b.end());
+        return a==b; 
+    }
+};
