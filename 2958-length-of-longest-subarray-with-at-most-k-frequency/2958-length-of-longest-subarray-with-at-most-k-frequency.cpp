846f696c573a04f34f6f9a8a265ecf56e2eca6a2
Bishal299 <bishalkhuntia23@gmail.com>
2026-08-12 16:14:45 +0530

    Time: 77 ms (20.67%), Space: 149.3 MB (84.27%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -6,14 +6,14 @@ public:
         unordered_map<int,int>m;
        // m.reserve(n);
         for(int l=0,i=0;i<n;i++){
-            // auto it=m.find(nums[i]);
-            // if(it != m.end()){
-            //     it->second ++;
+            auto it=m.find(nums[i]);
+            if(it != m.end()){
+                it->second ++;
 
-            // }else{
-            //     m[nums[i]]=1;
-            // }
-            m[nums[i]]++;
+            }else{
+                m[nums[i]]=1;
+            }
+           // m[nums[i]]++;
             while(m[nums[i]]>k){
                 m[nums[l]]--;
                 l++;
