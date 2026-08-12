3f67ba02a50895ee50aadc9ca40b438116985622
Bishal299 <bishalkhuntia23@gmail.com>
2026-08-12 16:15:05 +0530

    Time: 69 ms (37.25%), Space: 149.2 MB (99.16%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -6,14 +6,8 @@ public:
         unordered_map<int,int>m;
        // m.reserve(n);
         for(int l=0,i=0;i<n;i++){
-            auto it=m.find(nums[i]);
-            if(it != m.end()){
-                it->second ++;
-
-            }else{
-                m[nums[i]]=1;
-            }
-           // m[nums[i]]++;
+         
+           m[nums[i]]++;
             while(m[nums[i]]>k){
                 m[nums[l]]--;
                 l++;
