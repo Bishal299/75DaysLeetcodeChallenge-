6367dcc4fad4cd17c5711d7ca372fde17229c3af
Bishal299 <bishalkhuntia23@gmail.com>
2026-09-26 01:24:13 +0530

    Time: 0 ms (100%), Space: 26.3 MB (8.63%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,13 +6,10 @@ public:
         int high =n-1;
         while(low<high){
             int mid=low+(high-low)/2 ;
-            if(mid%2==1){
-                mid--;
-            }
-            if(nums[mid]==nums[mid+1]){
-                low=mid+2;
-            }
-            else{
+            // if(mid%2==1){}
+            if((mid%2==0 &&nums[mid]==nums[mid+1]) || (mid%2!=0 && nums[mid]==nums[mid-1]) ){
+                low=mid+1;
+            }else{
                 high=mid;
             }
           
